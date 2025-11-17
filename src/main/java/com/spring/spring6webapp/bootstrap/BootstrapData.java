d914ef6047cfb1b1586049dfa42bbd4c64ca72ef
Andrew Schwirzinski <aschwirzinski@gmail.com>
2025-11-17 12:51:10 -0500

    publisher entity and repository

--- src/main/java/com/spring/spring6webapp/bootstrap/BootstrapData.java
+++ src/main/java/com/spring/spring6webapp/bootstrap/BootstrapData.java
@@ -2,8 +2,10 @@ package com.spring.spring6webapp.bootstrap;
 
 import com.spring.spring6webapp.domain.Author;
 import com.spring.spring6webapp.domain.Book;
+import com.spring.spring6webapp.domain.Publisher;
 import com.spring.spring6webapp.repositories.AuthorRepository;
 import com.spring.spring6webapp.repositories.BookRepository;
+import com.spring.spring6webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,10 +14,12 @@ public class BootstrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private  final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -37,10 +41,18 @@ public class BootstrapData implements CommandLineRunner {
         noEJB.setTitle("J2ee Development without EJ8");
         noEJB.setIsbn("789123");
 
+        Publisher DomainPublishing = new Publisher();
+        DomainPublishing.setName("Domain Publishing" );
+        DomainPublishing.setAddress("111 2nd street");
+        DomainPublishing.setCity("Tampa");
+        DomainPublishing.setState("Florida");
+        DomainPublishing.setZip("45678");
+
         Author ericSaved = authorRepository.save(Eric);
         Author rodSaved = authorRepository.save(Rod);
         Book dddSaved = bookRepository.save(ddd);
         Book noEJBSaved = bookRepository.save(noEJB);
+        Publisher DomainPublishingSaved = publisherRepository.save(DomainPublishing);
 
         ericSaved.getBooks().add(dddSaved);
         rodSaved.getBooks().add(noEJBSaved);
@@ -51,6 +63,7 @@ public class BootstrapData implements CommandLineRunner {
         System.out.println("In bootstrap data");
         System.out.println("Author count: " + authorRepository.count());
         System.out.println("Book count: " + bookRepository.count());
+        System.out.println("Publisher count: " + publisherRepository.count());
 
 
     }
